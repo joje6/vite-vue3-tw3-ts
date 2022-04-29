3b8d8430323aa6fe6b257fc923181e81816ed67b
joje <joje.attrs@gmail.com>
2022-04-30 00:44:35 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  important: true,
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx,html}",
+  ],
+  theme: {},
+};
