271165e988e0ead0a9f687f96bb032dbec2bf302
joje <joje.attrs@gmail.com>
2022-05-02 17:56:51 +0900

    Add node polyfill

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import './registerServiceWorker';
 import './css/index.css';
+import stream from 'stream';
+import os from 'os';
+import url from 'url';
+import process from 'process';
+import buffer from 'buffer';
+import querystring from 'querystring';
+
+console.log('stream', stream);
+console.log('os', os);
+console.log('url', url);
+console.log('process', process);
+console.log('buffer', buffer);
+console.log('querystring', querystring);
 
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,31 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import svgLoader from 'vite-svg-loader';
+import viteSvgLoader from 'vite-svg-loader';
 import { resolve } from 'path';
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      '@': resolve(__dirname, 'src'),
+      'buffer': 'buffer',
+      'process': 'process',
+      'util': 'rollup-plugin-node-polyfills/polyfills/util',
+      'events': 'rollup-plugin-node-polyfills/polyfills/events',
+      'stream': 'rollup-plugin-node-polyfills/polyfills/stream',
+      'path': 'rollup-plugin-node-polyfills/polyfills/path',
+      'querystring': 'rollup-plugin-node-polyfills/polyfills/qs',
+      'url': 'rollup-plugin-node-polyfills/polyfills/url',
+      'os': 'rollup-plugin-node-polyfills/polyfills/os'
     }
   },
   plugins: [
     vue(),
-    svgLoader({
+    viteSvgLoader({
       defaultImport: 'component',
       svgo: false
     })
-  ]
+  ],
+  server: {
+    port: 3000
+  }
 });
